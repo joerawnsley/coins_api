b4d4c2b9d396ec7fd2af403125464297eded57e1
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-04 16:02:43 +0100

    create db connection

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-db = None
+from peewee import PostgresqlDatabase
+from os import getenv
+
+db = PostgresqlDatabase(
+    'joe',
+    user=getenv('DB_USER'),
+    host=getenv('DB_HOST'),
+    port=getenv('DB_PORT'),
+    password=getenv('DB_PASSWORD')
+    )
