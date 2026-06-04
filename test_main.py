726ff6baffe4f9ae0e8c864fd9af2d2c08e11384
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-04 19:18:23 +0100

    add test to check duties can be pulled from db

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from main import db
-from models import Coin
+from models import Coin, Duty
 
 def test_connection():
     connection = db.connect()
@@ -12,3 +12,13 @@ def test_pull_coin_from_db():
         first_coin = Coin.select().first()
         print(first_coin.coin_name)
         assert first_coin
+    if not db.is_closed():
+        db.close()
+    
+def test_pull_duty_from_db():
+    with db:
+        first_duty = Duty.select().first()
+        print(first_duty.description)
+        assert first_duty
+    if not db.is_closed():
+        db.close()
