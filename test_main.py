e05b18252ab3023d5786879c081969ac0101c4a8
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-04 16:52:23 +0100

    add test to pull coin from db

--- test_main.py
+++ test_main.py
@@ -1,5 +1,14 @@
 from main import db
+from models import Coin
 
 def test_connection():
     connection = db.connect()
     assert connection
+    if not db.is_closed():
+        db.close()
+
+def test_pull_coin_from_db():
+    with db:
+        first_coin = Coin.select().first()
+        print(first_coin.coin_name)
+        assert first_coin
