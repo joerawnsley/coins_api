384b26f727720b699f926b6e2fafb38889f1d454
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-05 12:46:44 +0100

    add test for adding a duty to empty db

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ def test_connection():
 
 
 @pytest.fixture()
-def test_database():
+def empty_database():
     with db:
         db.create_tables([Coin, Duty])
         
@@ -20,15 +20,21 @@ def test_database():
         
         db.drop_tables([Coin, Duty])
 
-def test_coin_table_is_empty(test_database):
+def test_coin_table_is_empty(empty_database):
     
     coins = Coin.select()
     assert list(coins) == []
     
-def test_add_a_coin(test_database):
+def test_add_a_coin(empty_database):
     Coin.insert(coin_name='Automate').execute()
     coin = Coin.select().first()
     print(f'name: {coin.coin_name}, id: {coin.id}')
     assert coin.id == 1
     assert coin.coin_name == 'Automate'
-    
+
+def test_add_a_duty(empty_database):
+    Duty.insert(duty_name='Duty 1', description='Script and code').execute()
+    duty = Duty.get_by_id(1)
+    print(f'name: {duty.duty_name}, id: {duty.id}, description: {duty.description}')
+    assert duty.id == 1
+    assert 'code' in duty.description
