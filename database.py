aad1f00a9610935603f45c269fdea8d0dc64717b
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-05 10:51:47 +0100

    add SQLite DB reference and toggle using .env variable

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
-from peewee import PostgresqlDatabase
+from peewee import PostgresqlDatabase, SqliteDatabase
 import os, dotenv
 
 dotenv.load_dotenv()
 
-db = PostgresqlDatabase(
+postgres_db = PostgresqlDatabase(
     'joe',
     user=os.getenv('DB_USER'),
     host=os.getenv('DB_HOST'),
     port=os.getenv('DB_PORT'),
     password=os.getenv('DB_PASSWORD')
     )
+
+sqlite_db = SqliteDatabase(':memory:')
+
+if os.getenv('DB_LOCATION') == 'REMOTE':
+    db = postgres_db
+if os.getenv('DB_LOCATION') == 'MEMORY':
+    db = sqlite_db
