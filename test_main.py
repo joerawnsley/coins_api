6f57847160217a69b9eb5d81c844bd13fae02b86
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-05 09:39:31 +0100

    update tests to check both id and name/description

--- test_main.py
+++ test_main.py
@@ -10,15 +10,17 @@ def test_connection():
 def test_pull_coin_from_db():
     with db:
         first_coin = Coin.select().first()
-        print(first_coin.coin_name)
-        assert first_coin
+        print(first_coin.id, first_coin.coin_name)
+        assert first_coin.id
+        assert first_coin.coin_name
     if not db.is_closed():
         db.close()
     
 def test_pull_duty_from_db():
     with db:
         first_duty = Duty.select().first()
-        print(first_duty.description)
-        assert first_duty
+        print(first_duty.id, first_duty.description)
+        assert first_duty.id
+        assert first_duty.description
     if not db.is_closed():
         db.close()
