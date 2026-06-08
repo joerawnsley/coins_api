cd5a3fedae6367bd490cac489d527e512d70e9de
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-08 22:33:17 +0100

    add test coins route returns list

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,3 +7,13 @@ def test_root_returns_message():
     response = client.get("/")
     assert response.status_code == 200
     assert "Welcome" in response.text
+
+def test_root_returns_json_object():
+    response = client.get("/")
+    data = response.json()
+    assert isinstance(data, dict)
+
+def test_coins_route_returns_a_list():
+    response = client.get("/coins")
+    data = response.json()
+    assert isinstance(data, list)
