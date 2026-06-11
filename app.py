4148be567143d15e75eaad42ead6c2cfdb6fa331
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-11 13:06:19 +0100

    implement explicit dictionary return from get route including duty numbers array

--- app.py
+++ app.py
@@ -9,11 +9,27 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to the Coins API"}
 
+
 @app.get("/coins")
 def list_coins():
-    query = Coin.select().dicts()
-    coin_list = [coin for coin in query]
-    for coin in coin_list:
-        coin["duties"] = []
+    query = Coin.select()
+    coin_list = []
+    for coin in query:
+        coin_list.append(dict(
+        id = coin.id,
+        coin_name = coin.coin_name,
+        duties = [duty.duty_number for duty in coin.duties],
+        is_complete = coin.is_complete
+    ))
     return coin_list
+
+
+
+# @app.get("/coins")
+# def list_coins():
+#     query = Coin.select().dicts()
+#     coin_list = [coin for coin in query]
+#     for coin in coin_list:
+#         coin["duties"] = []
+#     return coin_list
 
