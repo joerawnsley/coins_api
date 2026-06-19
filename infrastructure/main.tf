3042ef05f365637f826bd2b196e21b66ba4cab4c
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-19 07:42:21 +0100

    add force_delete to ecr

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_ecr_repository" "app_repo" {
   name                 = "jbr-coins-api"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
