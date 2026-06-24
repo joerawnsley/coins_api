941785c533eb270a83712e267b3dd6cd95415ace
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-24 17:19:55 +0100

    separate tests and fixtures

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,52 +13,6 @@ if os.getenv('DB_LOGGING') == 'on':
 # ------------ create test client ----------------
 client = TestClient(app)
 
-# --------------- test fixtures -----------------
-@pytest.fixture()
-def empty_database():
-    db.connect()
-    db.create_tables([Coin, Duty, Coin.duties.get_through_model()])
-    
-    yield
-    
-    db.drop_tables([Coin, Duty, Coin.duties.get_through_model()])
-    if not db.is_closed():
-        db.close()
-
-with open('seed_data/seed_data.json') as json_data:
-    seed_data = json.load(json_data)
-    all_coins = seed_data['coins']
-    all_duties = seed_data['duties']
-    
-@pytest.fixture()
-def full_database():
-    
-    db.connect()
-    db.create_tables([Coin, Duty, Coin.duties.get_through_model()])
-    Coin.insert_many(all_coins).execute()
-    Duty.insert_many(all_duties).execute()
-    
-    yield
-    
-    db.drop_tables([Coin, Duty, Coin.duties.get_through_model()])
-    
-    if not db.is_closed():
-        db.close()
-
-@pytest.fixture()
-def db_with_duties_but_no_coins():
-    
-    db.connect()
-    db.create_tables([Coin, Duty, Coin.duties.get_through_model()])
-    Duty.insert_many(all_duties).execute()
-    
-    yield
-    
-    db.drop_tables([Coin, Duty, Coin.duties.get_through_model()])
-    
-    if not db.is_closed():
-        db.close()
-
 # ----- welcome endpoint -----
 
 def test_root_returns_message():
